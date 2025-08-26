da0fd0db7d35c11edf4d06f9eb5d6c862af3a96c
Omarelghandour09 <005omar.elsayed005@gmail.com>
2025-08-26 14:15:10 +0300

    Add files via upload

---
+++ 0002.py
@@ -0,0 +1,9 @@
+f_name = "Omar"
+l_name = "ElGhandour"
+GPA = 3.98
+year = 2025
+day = 26
+month = 8
+date = str(year) + "-" + str(month) + "-" + str(day)
+age = year - 2005
+print(f"My name is {f_name} {l_name} , my age is {age} , my GPA is {GPA} and today's date is {date}")
